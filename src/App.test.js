f3a5d347fa7878cbd2c473aaad75c4247307782f
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-05-24 19:44:32 +0200

    refactor test to use describe and it instead of test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders menu', () => {
-  render(<App />);
-  const menu = screen.getByText("Menu");
-  expect(menu).toBeInTheDocument();
+describe("render App", () => {
+  it('render menu', () => {
+    render(<App />);
+    const menu = screen.getByText("Menu");
+    expect(menu).toBeInTheDocument();
+  });
 });
