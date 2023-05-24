f4acdf5b47ef61f8732ffae37976057784de4fca
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-05-25 00:13:37 +0200

    Green phase: button is disabled if checkbox is not checked

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -1,11 +1,15 @@
+import {useState} from "react";
+
 export function OrderSummary() {
+    const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+
     return <>
         <h1>Order Summary</h1>
         <div>
-            <button>Place your order</button>
+            <button disabled={!isCheckboxChecked}>Place your order</button>
         </div>
         <div>
-            <input type={"checkbox"} aria-labelledby={"tac"}/>
+            <input type={"checkbox"} aria-labelledby={"tac"} checked={isCheckboxChecked}/>
             <span id={"tac"}>Agree to Terms and Conditions</span>
             {/*
             Another way is this one
