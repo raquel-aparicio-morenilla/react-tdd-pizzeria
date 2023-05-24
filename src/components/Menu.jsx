7bbb8f57f9bfb5d109ac755dc3dd6d78f8023c9a
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-05-24 20:12:34 +0200

    Refactor phase: add a state to store the list of pizzas and add useEffect with condition to be executed only once, to avoid infinite loop

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,15 @@
+import {useEffect, useState} from "react";
+
 function getPizzaList() {
     return ["Carbonara", "Barbeque"];
 }
 
 export function Menu() {
-    const pizzaList = getPizzaList();
+    const [pizzaList, setPizzaList] = useState([])
+
+    useEffect(() => {
+        setPizzaList(getPizzaList());
+    }, [])
 
     return <>
         <h1>Menu</h1>
