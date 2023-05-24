27aec222fb1987877db703ad58de9c6bedfcea0e
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-05-24 23:44:49 +0200

    Red phase: expect "Order Summary" on App

--- src/App.test.js
+++ src/App.test.js
@@ -18,91 +18,99 @@ jest.mock("./gateways/menuGateway", () => ({
 );
 
 describe("render App", () => {
-  it('render menu', () => {
-    render(<App />);
-    const menu = screen.getByText("Menu");
-    expect(menu).toBeInTheDocument();
-  });
+  describe("render Menu section", () => {
+    it('render menu', () => {
+      render(<App />);
+      const menu = screen.getByText("Menu");
+      expect(menu).toBeInTheDocument();
+    });
 
-  it("render Pizza under Menu", () => {
-    render(<App/>)
-    const pizzaSection = screen.getByText("Pizza")
-    expect(pizzaSection).toBeInTheDocument()
+    it("render Pizza under Menu", () => {
+      render(<App/>)
+      const pizzaSection = screen.getByText("Pizza")
+      expect(pizzaSection).toBeInTheDocument()
+    })
+
+    it("render Carbonara in Pizza section", () => {
+      render(<App/>)
+      const carbonara = screen.getByText("Carbonara")
+      expect(carbonara).toBeInTheDocument()
+    })
+
+    it("render Barbeque in Pizza section", () => {
+      render(<App/>)
+      const barbeque = screen.getByText("Barbeque")
+      expect(barbeque).toBeInTheDocument()
+    })
+
+    it("render Dessert section in Menu", () =>{
+      render(<App/>)
+      const dessertSection = screen.getByText("Dessert")
+      expect(dessertSection).toBeInTheDocument()
+    })
+
+    it("render Vanilla icecream on the dessert section", () => {
+      render(<App/>)
+      const vanilla = screen.getByText("Vanilla icecream")
+      expect(vanilla).toBeInTheDocument()
+    })
+
+    it("render Vanilla icecream UNDER the dessert section", () => {
+      render(<App/>)
+      const dessertSection = screen.getByLabelText("dessertSection")
+      const vanilla = within(dessertSection).getByText("Vanilla icecream")
+      expect(vanilla).toBeInTheDocument()
+    })
+
+    it("render images for the 2 pizzas", () =>{
+      render(<App/>)
+      const imageList = screen.getAllByRole("img", {name: /pizza/i});
+      expect(imageList).toHaveLength(2)
+      const altList = imageList.map(pizzaImage => pizzaImage.alt)
+      expect(altList).toContain("Carbonara pizza")
+      expect(altList).toContain("Barbeque pizza")
+      expect(altList).toEqual(expect.arrayContaining(["Carbonara pizza", "Barbeque pizza"]))
+
+      const srcList = imageList.map(pizzaImage => pizzaImage.src)
+      expect(srcList).toEqual(expect.arrayContaining([expect.stringContaining("assets/Carbonara.png"), expect.stringContaining("assets/Barbeque.png")]))
+    })
+
+    it("renders images for the dessert", () =>{
+      render(<App/>)
+      const imageList= screen.getAllByRole("img", {name: /dessert/i})
+      expect(imageList).toHaveLength(1)
+      const altList = imageList.map(image => image.alt)
+      expect(altList).toEqual(["Vanilla icecream dessert"])
+      const srcList= imageList.map(image => image.src)
+      expect(srcList).toEqual([expect.stringContaining("assets/VanillaIcecream.png")])
+    })
+
+    it("renders spinners for the menu items with an initial value of 0", () => {
+      render(<App/>)
+      const spinnerList = screen.getAllByRole("spinbutton")
+      expect(spinnerList).toHaveLength(3)
+      spinnerList.forEach(spinner => expect(spinner.value).toBe("0"))
+    })
+
+    it("update spinner value with user input", async () => {
+      render(<App/>)
+      const spinnerCarbonara = screen.getByRole("spinbutton", {name: "Carbonara pizza"})
+      userEvent.type(spinnerCarbonara,"3")
+      await waitFor(() => expect(spinnerCarbonara.valueAsNumber).toBe(3))
+    })
+
+    it("renders price for the menu items", () => {
+      render(<App/>)
+      const priceList = screen.getAllByLabelText(/-price$/)
+      expect(priceList).toHaveLength(3)
+      const carbonaraPrice = screen.getByLabelText("Carbonara-pizza-price")
+      expect(carbonaraPrice).toHaveTextContent("15")
+    })
   })
 
-  it("render Carbonara in Pizza section", () => {
+  it("render Summary", () => {
     render(<App/>)
-    const carbonara = screen.getByText("Carbonara")
-    expect(carbonara).toBeInTheDocument()
-  })
-
-  it("render Barbeque in Pizza section", () => {
-    render(<App/>)
-    const barbeque = screen.getByText("Barbeque")
-    expect(barbeque).toBeInTheDocument()
-  })
-
-  it("render Dessert section in Menu", () =>{
-    render(<App/>)
-    const dessertSection = screen.getByText("Dessert")
-    expect(dessertSection).toBeInTheDocument()
-  })
-
-  it("render Vanilla icecream on the dessert section", () => {
-    render(<App/>)
-    const vanilla = screen.getByText("Vanilla icecream")
-    expect(vanilla).toBeInTheDocument()
-  })
-
-  it("render Vanilla icecream UNDER the dessert section", () => {
-    render(<App/>)
-    const dessertSection = screen.getByLabelText("dessertSection")
-    const vanilla = within(dessertSection).getByText("Vanilla icecream")
-    expect(vanilla).toBeInTheDocument()
-  })
-
-  it("render images for the 2 pizzas", () =>{
-    render(<App/>)
-    const imageList = screen.getAllByRole("img", {name: /pizza/i});
-    expect(imageList).toHaveLength(2)
-    const altList = imageList.map(pizzaImage => pizzaImage.alt)
-    expect(altList).toContain("Carbonara pizza")
-    expect(altList).toContain("Barbeque pizza")
-    expect(altList).toEqual(expect.arrayContaining(["Carbonara pizza", "Barbeque pizza"]))
-
-    const srcList = imageList.map(pizzaImage => pizzaImage.src)
-    expect(srcList).toEqual(expect.arrayContaining([expect.stringContaining("assets/Carbonara.png"), expect.stringContaining("assets/Barbeque.png")]))
-  })
-
-  it("renders images for the dessert", () =>{
-    render(<App/>)
-    const imageList= screen.getAllByRole("img", {name: /dessert/i})
-    expect(imageList).toHaveLength(1)
-    const altList = imageList.map(image => image.alt)
-    expect(altList).toEqual(["Vanilla icecream dessert"])
-    const srcList= imageList.map(image => image.src)
-    expect(srcList).toEqual([expect.stringContaining("assets/VanillaIcecream.png")])
-  })
-
-  it("renders spinners for the menu items with an initial value of 0", () => {
-    render(<App/>)
-    const spinnerList = screen.getAllByRole("spinbutton")
-    expect(spinnerList).toHaveLength(3)
-    spinnerList.forEach(spinner => expect(spinner.value).toBe("0"))
-  })
-
-  it("update spinner value with user input", async () => {
-    render(<App/>)
-    const spinnerCarbonara = screen.getByRole("spinbutton", {name: "Carbonara pizza"})
-    userEvent.type(spinnerCarbonara,"3")
-    await waitFor(() => expect(spinnerCarbonara.valueAsNumber).toBe(3))
-  })
-
-  it("renders price for the menu items", () => {
-    render(<App/>)
-    const priceList = screen.getAllByLabelText(/-price$/)
-    expect(priceList).toHaveLength(3)
-    const carbonaraPrice = screen.getByLabelText("Carbonara-pizza-price")
-    expect(carbonaraPrice).toHaveTextContent("15")
+    const summary = screen.getByText("Order Summary")
+    expect(summary).toBeInTheDocument()
   })
 });
