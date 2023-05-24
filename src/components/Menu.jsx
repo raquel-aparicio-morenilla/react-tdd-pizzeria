eeb5f3cf35d83c2a8627e1622fa8b0fb7b0029f6
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-05-24 20:32:05 +0200

    Green phase: show dessert section in the Menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -7,5 +7,6 @@ export function Menu() {
         <h1>Menu</h1>
         <h2>Pizza</h2>
         {pizzaList.map(pizza => <div key={pizza.name}>{pizza.name}</div>)}
+        <h2>Dessert</h2>
     </>;
 }
