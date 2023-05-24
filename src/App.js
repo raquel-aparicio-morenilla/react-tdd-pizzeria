ef1a9188226978da13045cd823f099a72ef5be15
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-05-24 23:57:08 +0200

    Green phase: add checkbox for Terms and Conditions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
         <Menu/>
         Order Summary
         <button>Place your order</button>
+        <input type={"checkbox"} aria-labelledby={"tac"}/>
+        <span id={"tac"}>Agree to Terms and Conditions</span>
+        {/*
+        Another way is this one
+        <label><input type={"checkbox"}/>Agree to Terms and Conditions</label>
+        */}
+
     </div>
   );
 }
