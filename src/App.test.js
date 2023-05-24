e065b157a00017a4100c45807466d03b86804d44
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-05-24 19:40:56 +0200

    RED phase: create failing test for render Menu on App

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders menu', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const menu = screen.getByText("Menu");
+  expect(menu).toBeInTheDocument();
 });
