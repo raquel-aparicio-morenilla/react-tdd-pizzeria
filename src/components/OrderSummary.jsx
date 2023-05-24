e48bcbcd73afeecb6a0f1a90970dd50cc964bf79
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-05-25 00:28:33 +0200

    Green phase: render Your order has been confirmed only when order button is clicked

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -1,15 +1,27 @@
 import {useState} from "react";
 
+const orderStatePendingToConfirm = 'pendingToConfirm'
+const orderStateConfirmed = 'confirmed'
+
 export function OrderSummary() {
     const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
+    const [orderState, setOrderState] = useState(orderStatePendingToConfirm)
+
+    const handleButtonClick = () => {
+        setOrderState(orderStateConfirmed)
+    }
 
     const handleCheckboxChange = () => {
         setIsCheckboxChecked(!isCheckboxChecked)
     }
+
+    const isOrderConfirmed = orderState === orderStateConfirmed;
+
     return <>
         <h1>Order Summary</h1>
+        {isOrderConfirmed && <div>Your order has been confirmed</div>}
         <div>
-            <button disabled={!isCheckboxChecked}>Place your order</button>
+            <button disabled={!isCheckboxChecked} onClick={handleButtonClick}>Place your order</button>
         </div>
         <div>
             <input type={"checkbox"} aria-labelledby={"tac"} checked={isCheckboxChecked} onChange={handleCheckboxChange}/>
