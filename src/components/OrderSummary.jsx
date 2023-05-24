7ea3cce54c604032af8418d5ce9f571c82f74d18
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-05-25 00:14:27 +0200

    Green phase: button is enabled when checkbox is checked

--- src/components/OrderSummary.jsx
+++ src/components/OrderSummary.jsx
@@ -3,13 +3,16 @@ import {useState} from "react";
 export function OrderSummary() {
     const [isCheckboxChecked, setIsCheckboxChecked] = useState(false);
 
+    const handleCheckboxChange = () => {
+        setIsCheckboxChecked(!isCheckboxChecked)
+    }
     return <>
         <h1>Order Summary</h1>
         <div>
             <button disabled={!isCheckboxChecked}>Place your order</button>
         </div>
         <div>
-            <input type={"checkbox"} aria-labelledby={"tac"} checked={isCheckboxChecked}/>
+            <input type={"checkbox"} aria-labelledby={"tac"} checked={isCheckboxChecked} onChange={handleCheckboxChange}/>
             <span id={"tac"}>Agree to Terms and Conditions</span>
             {/*
             Another way is this one
