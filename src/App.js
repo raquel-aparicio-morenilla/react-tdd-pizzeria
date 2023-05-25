0f2cbf3ca9d2b86e3f542ec02f09e888b3556f41
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-05-25 22:09:23 +0200

    Refactor: take custom hook to its own file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import './App.css';
 import {Menu} from "./components/Menu";
 import {OrderSummary} from "./components/OrderSummary";
-import {useState} from "react";
-
-function useShoppingCart() {
-    const [shoppingCartMap, setShoppingCartMap] = useState(new Map())
-
-    const updateShoppingCart = ({item, itemCount}) => {
-        const newShoppingCartMap = new Map(shoppingCartMap)
-        itemCount == 0 ? newShoppingCartMap.delete(item) : newShoppingCartMap.set(item, itemCount)
-        setShoppingCartMap(newShoppingCartMap)
-    }
-
-    const shoppingCart = [...shoppingCartMap].map(([mapKey, mapValue]) => ({item: mapKey, itemCount: mapValue}))
-    return {updateShoppingCart, shoppingCart};
-}
+import {useShoppingCart} from "./hooks/useShoppingCart";
 
 function App() {
     const {updateShoppingCart, shoppingCart} = useShoppingCart();
