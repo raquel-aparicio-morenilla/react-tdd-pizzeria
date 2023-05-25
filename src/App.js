994311484fda33a51c8c1c87ba087038342c0c62
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-05-25 22:07:24 +0200

    Refactor: extract custom hook for shopping cart

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import {Menu} from "./components/Menu";
 import {OrderSummary} from "./components/OrderSummary";
 import {useState} from "react";
 
-function App() {
+function useShoppingCart() {
     const [shoppingCartMap, setShoppingCartMap] = useState(new Map())
 
     const updateShoppingCart = ({item, itemCount}) => {
         const newShoppingCartMap = new Map(shoppingCartMap)
-        itemCount == 0 ? newShoppingCartMap.delete(item) : newShoppingCartMap.set(item,itemCount)
+        itemCount == 0 ? newShoppingCartMap.delete(item) : newShoppingCartMap.set(item, itemCount)
         setShoppingCartMap(newShoppingCartMap)
     }
 
-    const shoppingCart = [...shoppingCartMap].map(([mapKey, mapValue]) => ({ item: mapKey, itemCount: mapValue }))
+    const shoppingCart = [...shoppingCartMap].map(([mapKey, mapValue]) => ({item: mapKey, itemCount: mapValue}))
+    return {updateShoppingCart, shoppingCart};
+}
+
+function App() {
+    const {updateShoppingCart, shoppingCart} = useShoppingCart();
 
     return (
     <div className="App">
