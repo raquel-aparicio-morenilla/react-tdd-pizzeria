ee7d9f8b9b36ddaaf7c9149f4c9860fb3eb8069b
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-06-02 17:20:11 +0200

    greeb phase render price and spinbutton for each menu item

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,9 +1,21 @@
+import {useState} from "react";
+
 export const pizzaType ="pizza"
 export const dessertType = "dessert"
 
 export function MenuItem({item, itemType}) {
+    const [quantity, setQuantity] = useState(0)
+
+    const handleChange = (ev) => {
+        const newQuantity = parseInt(ev.target.valueAsNumber) || 0
+        setQuantity(newQuantity)
+    }
+
     return <div>
         <span><img width={50} height={50} src={"assets/"+item.imageName} alt={"Deliciouss " + item.name + " " +itemType}/></span>
         {item.name}
+
+        <span aria-label={"price"}>Price: {item.price}</span>
+        <input data-testid={item.name + "-spinbutton"} type={"number"} min ={0} value={quantity} onChange={handleChange}/>
     </div>;
 }
