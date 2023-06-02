3cca29d3b85d6f5d62e12aa94b5a310131e7d0e6
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-06-02 17:20:01 +0200

    red phase expect price and spinbutton for each menu item

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
-import {render, screen, within} from '@testing-library/react';
+import {render, screen, waitFor, within} from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
 jest.mock("./gateways/menuGateway", () => ({
     retrievePizzaList : () => (
@@ -73,6 +74,30 @@ describe("render Application", () => {
     })
     it('should render the price for each menu item', function () {
         render(<App/>)
+        const priceList = screen.getAllByLabelText("price")
+    });
 
+  it("should render spinbuttons on each menu item", ()=> {
+      render(<App/>)
+      const spinbuttonList = screen.getAllByRole("spinbutton");
+      expect(spinbuttonList).toHaveLength(3)
+      spinbuttonList.forEach(
+          spinbutton => expect(spinbutton.value).toBe("0")
+      )
+  })
+
+    it('should accept user input', async function () {
+        render(<App/>)
+        const spinbutton = screen.getByTestId("Carbonara-spinbutton")
+        userEvent.clear(spinbutton)
+        userEvent.type(spinbutton, "1");
+        await waitFor(() => expect(spinbutton.valueAsNumber).toBe(1));
+    });
+
+    it('should not allow negative numbers or empty, having 0 as default', async function () {
+        render(<App/>)
+        const spinbutton = screen.getByTestId("Carbonara-spinbutton")
+        userEvent.clear(spinbutton)
+        await waitFor(() => expect(spinbutton.valueAsNumber).toBe(0));
     });
 })
