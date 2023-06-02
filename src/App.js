dce94188752820ecab6d78b29c02897b306f217a
raquel.aparicio <raquel.aparicio@thoughtworks.com>
2023-06-02 16:33:17 +0200

    green phase show images on the menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useMenu} from "./hooks/useMenu";
-import {MenuItem} from "./components/MenuItem";
+import {dessertType, MenuItem, pizzaType} from "./components/MenuItem";
 
 function App() {
     const { pizzaList, dessertList } = useMenu();
@@ -10,9 +10,9 @@ function App() {
         <div aria-label={"Menu"}>
             <h1>Menu</h1>
             <h2>Pizza</h2>
-            {pizzaList.map(pizza => <MenuItem key={pizza} item={pizza}/>)}
+            {pizzaList.map(pizza => <MenuItem key={pizza.name} item={pizza} itemType={pizzaType}/>)}
             <h2>Dessert</h2>
-            {dessertList.map(dessert => <MenuItem key={dessert} item={dessert}/>)}
+            {dessertList.map(dessert => <MenuItem key={dessert.name} item={dessert} itemType={dessertType}/>)}
         </div>
     </div>
   );
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,3 +1,9 @@
-export function MenuItem({item}) {
-    return <div>{item}</div>;
+export const pizzaType ="pizza"
+export const dessertType = "dessert"
+
+export function MenuItem({item, itemType}) {
+    return <div>
+        <span><img width={50} height={50} src={"assets/"+item.imageName} alt={"Deliciouss " + item.name + " " +itemType}/></span>
+        {item.name}
+    </div>;
 }
